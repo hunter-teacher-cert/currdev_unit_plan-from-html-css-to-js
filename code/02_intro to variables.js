663c61283dfe50e8581b60c7029e93b92e359843
daiana munoz <85325660+daianam08@users.noreply.github.com>
2021-11-17 11:56:05 -0500

    Update and rename 01_code.md to 02_intro to variables.js

---
+++ code/02_intro to variables.js
@@ -0,0 +1,10 @@
+console.log("Hello");
+// Let's practice making variables below!
+
+// change birthYear to the year you were born
+// change toYear to current year
+var birthYear=2005;
+var toYear=2021;
+var age= toYear-birthYear;
+
+console.log("You are " + age + " years old!");
