c8ac4e4d726d463d37c1d5b318346db2781d4ef4
daiana munoz <85325660+daianam08@users.noreply.github.com>
2021-12-04 12:44:32 -0500

    Create 05_innerHTML and madlibs.js

---
+++ code/05_innerHTML and madlibs.js
@@ -0,0 +1,13 @@
+// STEP 1: We pull all the IDs we can find in the index and store them in their own variable. We cannot just use them as is because of the symbols attached to IDs and Classes. Find the rest!
+let adj1= document.querySelector("#adjective1");
+let adj2= ???;
+let noun1= ???;
+let noun2= ???;
+
+// STEP 2: We use the new variables and assign them some fun words using innerHTML. What would you like to add?
+adj1.innerHTML="???";
+adj2.innerHTML="???";
+noun1.innerHTML="???";
+noun2.innerHTML="???";
+
+// STEP 3: Run the program and see your silly madlib!
