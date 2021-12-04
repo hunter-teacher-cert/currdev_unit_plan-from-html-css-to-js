c7cfe72841da60555d8dd9d07be7f565bbfdc5f8
daiana munoz <85325660+daianam08@users.noreply.github.com>
2021-12-04 12:36:47 -0500

    Update 02_intro to variablesANSWER.js

--- code/02_intro to variablesANSWER.js
+++ code/02_intro to variablesANSWER.js
@@ -5,6 +5,8 @@ console.log("Hello");
 // change toYear to current year
 var birthYear=2005;
 var toYear=2021;
+//Create a new variable called age to subtract both birthYear and toYear
 var age= toYear-birthYear;
 
+// Add the variable age to the following console.log
 console.log("You are " + age + " years old!");
