1a3f1968d40797d973c4dee25b8c8462ba6b5373
daiana munoz <85325660+daianam08@users.noreply.github.com>
2021-12-04 12:39:01 -0500

    Create 03_intro to conditionals.js

---
+++ code/03_intro to conditionals.js
@@ -0,0 +1,16 @@
+console.log("Hello, I work!");
+
+  //--------------------------------------
+//Let's practice conditionals below! DO NOT TOUCH CODE ABOVE THIS LINE!!!
+
+var randomNumber = Math.floor((Math.random()*10)+1);
+
+var guess= ???;
+
+if (condition){
+???
+} else if (condition){
+???
+} else {
+???
+}
