18a3ad1340b608054275842d8cf18e5fce9b39e9
daiana munoz <85325660+daianam08@users.noreply.github.com>
2021-12-04 13:14:18 -0500

    Update 05_innerHTML and madlibsANSWER.js

--- code/05_innerHTML and madlibsANSWER.js
+++ code/05_innerHTML and madlibsANSWER.js
@@ -1,7 +1,10 @@
-// STEP 1: We pull all the IDs we can find in the index and store them in their own variable. We cannot just use them as is because of the symbols attached to IDs and Classes. Find the rest!
+// STEP 1: Let's set up window.prompt() again and ask the user for a noun. We will store it in a variable called userInput.
 var userInput = window.prompt("Give a noun");
+
+// Step 2: We need to store our user input into a variable for our first noun. If we don't store the input, next time we type into the console, we wil lose our information.
 var firstNoun= userInput;
-console.log(firstNoun)
+
+// Step 3: Rinse and repeat! Let's do the same for our adjectives and second noun. No need to make a new variable for userInput. Just call and store our next window.prompt().
 userInput= window.prompt("Give an adjective");
 var firstAdj= userInput;
 userInput= window.prompt("Give another noun");
@@ -9,15 +12,16 @@ var secondNoun= userInput;
 userInput= window.prompt("Give another adjective");
 var secondAdj= userInput;
 
+// STEP 4: We pull all the IDs we can find in the index and store them in their own variable. We cannot just use them as is because of the symbols attached to IDs and Classes. Find the rest!
 var adj1= document.querySelector("#adjective1");
 var adj2= document.querySelector("#adjective2");
 var noun1= document.querySelector("#noun1");
 var noun2= document.querySelector("#noun2");
 
-// STEP 2: We use the new variables and assign them some fun words using innerHTML. What would you like to add?
+// STEP 5: We use the new variables and assign them some fun words using innerHTML. What would you like to add?
 adj1.innerHTML=firstAdj;
 adj2.innerHTML=secondAdj ;
 noun1.innerHTML=firstNoun;
 noun2.innerHTML=secondNoun;
 
-// STEP 3: Run the program and see your silly madlib!
+// STEP 6: Run the program and see your silly madlib!
