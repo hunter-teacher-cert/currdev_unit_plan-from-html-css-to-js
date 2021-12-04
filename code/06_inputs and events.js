a34eb749ce3076adae97b690ac7aba450e29b84f
daiana munoz <85325660+daianam08@users.noreply.github.com>
2021-12-04 13:21:46 -0500

    Create 06_inputs and events.js

---
+++ code/06_inputs and events.js
@@ -0,0 +1,22 @@
+// STEP 1: Grab your IDs from the inputs and the div
+let ???;
+let ???;
+let ???;
+let ???;
+//the variable below is for the incorrect password counter for step 4
+let wrong=0;
+
+// STEP 2: CREATE THE EVENT LISTENER FOR THE BUTTON.
+???.addEventListener("click", e=>{
+// STEP 3: IF THE PASSWORD AND USERNAME ARE CORRECT, USE innerHTML ON THE LOG IN VARIABLE TO DISPLAY THAT YOU HAVE LOGGED IN. ELSE, DISPLAY THAT THEY SHOULD TRY AGAIN!
+  if (???){
+    ???
+  } else{
+    ???
+// STEP 4: LET'S REMEMBER TO ADD A COUNTER. IF THE OF TRIES REACHES 3, DISPLAY ACCOUNT LOCKED ERROR MESSAGE BELOW.
+    if(???){
+
+    clickButton.parentNode.removeChild(clickButton);
+    }
+  }
+})
