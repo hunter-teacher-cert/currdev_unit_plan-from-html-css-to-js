60cb5961d3c45d22dea7bb3233ce33ec0ebbb0b7
daiana munoz <85325660+daianam08@users.noreply.github.com>
2021-12-04 13:10:33 -0500

    Update 05_innerHTML and madlibsANSWER.js

--- code/05_innerHTML and madlibsANSWER.js
+++ code/05_innerHTML and madlibsANSWER.js
@@ -1,1 +1,23 @@
-??
+// STEP 1: We pull all the IDs we can find in the index and store them in their own variable. We cannot just use them as is because of the symbols attached to IDs and Classes. Find the rest!
+var userInput = window.prompt("Give a noun");
+var firstNoun= userInput;
+console.log(firstNoun)
+userInput= window.prompt("Give an adjective");
+var firstAdj= userInput;
+userInput= window.prompt("Give another noun");
+var secondNoun= userInput;
+userInput= window.prompt("Give another adjective");
+var secondAdj= userInput;
+
+var adj1= document.querySelector("#adjective1");
+var adj2= document.querySelector("#adjective2");
+var noun1= document.querySelector("#noun1");
+var noun2= document.querySelector("#noun2");
+
+// STEP 2: We use the new variables and assign them some fun words using innerHTML. What would you like to add?
+adj1.innerHTML=firstAdj;
+adj2.innerHTML=secondAdj ;
+noun1.innerHTML=firstNoun;
+noun2.innerHTML=secondNoun;
+
+// STEP 3: Run the program and see your silly madlib!
