e1729211b6a599cb9d7300777f8a0977f358e355
daiana munoz <85325660+daianam08@users.noreply.github.com>
2021-12-04 13:18:55 -0500

    Update 05_innerHTML and madlibs.js

--- code/05_innerHTML and madlibs.js
+++ code/05_innerHTML and madlibs.js
@@ -1,13 +1,27 @@
-// STEP 1: We pull all the IDs we can find in the index and store them in their own variable. We cannot just use them as is because of the symbols attached to IDs and Classes. Find the rest!
-let adj1= document.querySelector("#adjective1");
-let adj2= ???;
-let noun1= ???;
-let noun2= ???;
+// STEP 1: Let's set up window.prompt() again and ask the user for a noun. We will store it in a variable called userInput.
+var userInput =???;
 
-// STEP 2: We use the new variables and assign them some fun words using innerHTML. What would you like to add?
-adj1.innerHTML="???";
-adj2.innerHTML="???";
-noun1.innerHTML="???";
-noun2.innerHTML="???";
+// Step 2: We need to store our user input into a variable for our first noun. If we don't store the input, next time we type into the console, we wil lose our information.
+var ???= ???;
 
-// STEP 3: Run the program and see your silly madlib!
+// Step 3: Rinse and repeat! Let's do the same for our adjectives and second noun. No need to make a new variable for userInput. Just call and store our next window.prompt().
+userInput=???;
+var ???;
+userInput= ???;
+var ???;
+userInput= ???;
+var ???;
+
+// STEP 4: We pull all the IDs we can find in the index and store them in their own variable. We cannot just use them as is because of the symbols attached to IDs and Classes. Find the rest!
+var ???= document.querySelector("???");
+var ???;
+var ???;
+var ???;
+
+// STEP 5: We will use innerHTML to insert the userInput that we stored in the variables in steps 2 and 3. The first is done for you. Fill in the rest!
+adj1.innerHTML=firstAdj;
+adj2.innerHTML=???;
+noun1.innerHTML=???;
+noun2.innerHTML=???;
+
+// STEP 6: Run the program and see your silly madlib!
