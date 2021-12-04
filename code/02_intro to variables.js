ed085a02100dc0b2b4f37e4364ff6f679ef22483
daiana munoz <85325660+daianam08@users.noreply.github.com>
2021-12-04 12:36:02 -0500

    Create 02_intro to variables.js

---
+++ code/02_intro to variables.js
@@ -0,0 +1,11 @@
+console.log("Hello");
+// Let's practice making variables below!
+
+// change birthYear to the year you were born
+// change toYear to current year
+var birthYear=???;
+var toYear=???;
+//Create a new variable called age to subtract both birthYear and toYear
+
+// Add the variable age to the following console.log
+console.log("You are " + ??? + " years old!");
