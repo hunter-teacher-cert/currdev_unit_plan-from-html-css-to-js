cd78c7da97a075f6b3912bb13783e3eef3b470d6
daiana munoz <85325660+daianam08@users.noreply.github.com>
2021-12-04 12:40:15 -0500

    Create 04_strings and commandprompts.js

---
+++ code/04_strings and commandprompts.js
@@ -0,0 +1,13 @@
+// here we create an empty string variable and create a variable that controls the window prompt input. PLEASE DO NOT CHANGE THIS CODE!
+var boroughCheck="";
+var userInput = window.prompt("Name a borough!");
+
+//Step 2 below (we set up what happens when nothing is inputted)
+if ((userInput==null)||(userInput=="")){
+  boroughCheck="<h1>Nothing was entered. Refresh the page and try again.</h1>";
+} else if (???) {
+ ???
+} else{
+  ???
+}
+document.write(boroughCheck);
