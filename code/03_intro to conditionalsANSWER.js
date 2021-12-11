611c5441c0f19eab466ecaef7c36114ab07176b8
daiana munoz <85325660+daianam08@users.noreply.github.com>
2021-12-11 12:42:21 -0500

    Update 03_intro to conditionalsANSWER.js

--- code/03_intro to conditionalsANSWER.js
+++ code/03_intro to conditionalsANSWER.js
@@ -14,3 +14,17 @@ console.log("your guess is too high!");
 } else {
 console.log("you guessed too low!");
 }
+
+// Homework for lesson 2, lab 2
+// Now that we learned a little about creating variables, create a program in Javascript that converts a value from a variable
+// from one measurement to another. Print this in the console in a sentence. Remember tat variables can do all kinds of calculations.
+// Use the w3schools or our slides as a guide. Below is a list of examples:
+//  * feet to inches
+//  * Celcius to Farenheit or Kelvin
+//  * meters to feet
+
+//*Possible Answer*:
+
+//var feet= 10; var toInches= feet * 12;
+
+//console.log(feet+ " feet to inches = " + toInches);
